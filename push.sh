526401c481c017c3d3a8b78b6c9ead48f00cb831
MrKaplan <169855803+MrKaplan-lw@users.noreply.github.com>
2025-04-14 00:16:09 +0200

    Update crawler, readme, migrate most logic into GHA steps

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Purpose: a stupid git wrapper to deal with conflicts of ephemeral files
-#          when we push to GitHub
-git pull
-git checkout --theirs README.md
-git checkout --theirs awesome-lemmy-instances.csv
-git commit -am 'merge from GitHub'
-git push
